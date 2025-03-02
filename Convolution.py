67c73152a45ef9ad4d7faa5cfbcb15119063e8bc
Aaryan Patel <aaryangpat3l@gmail.com>
2025-03-02 09:00:22 -0500

    Refactored

--- Convolution.py
+++ Convolution.py
@@ -30,14 +30,14 @@ class Convolution:
         return self.output
 
     def backward(self, output_grad, learning_rate):
-        kernel_grad = np.zeros_like(self.kernel_shape)
-        input_grad = np.zeros_like(self.input_shape)
+        kernel_grad = np.zeros_like(self.kernels)
+        input_grad = np.zeros_like(self.input)
 
         for neuron in range(self.output_depth):
             for channel in range(self.input_depth):
-                kernel_grad[neuron, channel] = signal.correlate2d(self.input[channel], output_grad[neuron, channel],
+                kernel_grad[neuron, channel] += signal.correlate2d(self.input[channel], output_grad[neuron],
                                                                   "valid")
-                input_grad[channel] = signal.convolve2d(output_grad[neuron], self.kernels[neuron, channel], "full")
+                input_grad[channel] += signal.convolve2d(output_grad[neuron], self.kernels[neuron, channel], "full")
 
         self.kernels -= learning_rate * kernel_grad
         self.biases -= learning_rate * output_grad
