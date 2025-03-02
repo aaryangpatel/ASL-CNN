1b64f3bb90eea221909b0bd9d618eaf429326bc1
Aaryan Patel <aaryangpat3l@gmail.com>
2025-03-01 22:44:27 -0500

    Correct method signature

--- Sigmoid.py
+++ Sigmoid.py
@@ -5,5 +5,5 @@ class Sigmoid:
     def forward(self, input):
         return 1 / (1 + np.exp(-input))
 
-    def backward(self, input):
+    def backward(self, input, learning_rate):
         return self.forward(input) * (1 - self.forward(input))
