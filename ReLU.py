e6ad043b566fbfb58d5fd487581f70f85f642d07
Aaryan Patel <aaryangpat3l@gmail.com>
2025-03-02 11:20:02 -0500

    Added back propagation

--- ReLU.py
+++ ReLU.py
@@ -4,3 +4,8 @@ import numpy as np
 class ReLU:
     def forward(self, input):
         return max(0, input)
+
+    def backward(self, input, learning_rate):
+        if input <= 0:
+            return 0
+        return 1
