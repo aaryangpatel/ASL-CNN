cea810a277c4279337dc5e8bd7a1dcf870806ea6
Aaryan Patel <aaryangpat3l@gmail.com>
2025-03-02 15:18:59 -0500

    Fixed to apply to the entire input

--- ReLU.py
+++ ReLU.py
@@ -2,10 +2,13 @@ import numpy as np
 
 
 class ReLU:
+    def __init__(self):
+        self.input = None
+
     def forward(self, input):
+        self.input = input
         return np.maximum(0, input)
 
-    def backward(self, input, learning_rate):
-        if input <= 0:
-            return 0
-        return 1
+    def backward(self, output_grad, learning_rate):
+        grad_relu = np.where(self.input > 0, 1, 0)
+        return output_grad * grad_relu
