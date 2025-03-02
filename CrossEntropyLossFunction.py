c163fe607b5c1e532940e6852d4697af4908ffa7
Aaryan Patel <aaryangpat3l@gmail.com>
2025-03-01 22:42:58 -0500

    Add epsilon value to avoid log0

--- CrossEntropyLossFunction.py
+++ CrossEntropyLossFunction.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class CrossEntropyLossFunction:
     def forward(self, y_true, y_pred):
-        return -np.sum(y_true * np.log(y_pred))
+        return -np.sum(y_true * np.log(y_pred + 1e-10))
     
     def backward(self, y_true, y_pred):
         return y_pred - y_true
