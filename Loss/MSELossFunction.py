c42fe274a118435b0fb33c84303b3614c710fdb3
Aaryan Patel <aaryangpat3l@gmail.com>
2025-03-03 13:22:01 -0500

    Loss function updates

--- Loss/MSELossFunction.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-class MSELossFunction:
-    def mse(y_true, y_pred):
-        return np.mean((y_true - y_pred) ** 2)
-    
-    def d_mse(y_true, y_pred):
-        return 2 * (y_pred - y_true) / np.size(y_true)
