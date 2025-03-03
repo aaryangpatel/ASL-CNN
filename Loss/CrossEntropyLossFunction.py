a5e5f2512db54da338fc373e99dcb3958973d27c
Aaryan Patel <aaryangpat3l@gmail.com>
2025-03-02 22:39:02 -0500

    Loss directory

---
+++ Loss/CrossEntropyLossFunction.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+
+class CrossEntropyLossFunction:
+    """This class implements the cross entropy loss function which is used for training and evaluating the model."""
+    def forward(self, y_true, y_pred):
+        return -np.sum(y_true * np.log(y_pred + 1e-10))
+
+    def backward(self, y_true, y_pred):
+        return y_pred - y_true
