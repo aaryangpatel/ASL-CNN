91765ef16287ce332383136e058d5a8dfb043c57
Aaryan Patel <aaryangpat3l@gmail.com>
2025-03-02 22:32:53 -0500

    Comments and clean

--- ReLU.py
+++ ReLU.py
@@ -2,13 +2,17 @@ import numpy as np
 
 
 class ReLU:
+    """This class implements the Rectified Linear Unit (ReLU) activation function used throughout the network."""
     def __init__(self):
         self.input = None
 
+    """ReLU function for forward propagation."""
     def forward(self, input):
         self.input = input
         return np.maximum(0, input)
 
+    """ReLU function for back propagation."""
     def backward(self, output_grad, learning_rate):
-        grad_relu = np.where(self.input > 0, 1, 0)
-        return output_grad * grad_relu
+        # Generates new np array with 0s and 1s representing the result of the given condition
+        relu_grad = np.where(self.input > 0, 1, 0)
+        return output_grad * relu_grad
