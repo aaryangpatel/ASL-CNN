6e3dcd54609d32f8ca6edfb5d6f14886bb6af967
Aaryan Patel <aaryangpat3l@gmail.com>
2025-03-02 22:33:47 -0500

    Comments

--- Flatten.py
+++ Flatten.py
@@ -2,12 +2,15 @@ import numpy as np
 
 
 class Flatten:
+    """This class implements the Flatten layer which converts a multidimensional array into a 1D array."""
     def __init__(self):
         self.input_shape = None
 
+    """Flattens input into a 1D array."""
     def forward(self, input):
         self.input_shape = input.shape
         return input.flatten()
 
+    """Converts a flattened array back into the original input's shape."""
     def backward(self, output_grad, learning_rate):
         return output_grad.reshape(self.input_shape)
