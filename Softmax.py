b8550f8e308d0f8d09488ea8d0765613efbba8d7
Aaryan Patel <aaryangpat3l@gmail.com>
2025-03-02 22:34:39 -0500

    Comments and new back propagation

--- Softmax.py
+++ Softmax.py
@@ -2,12 +2,16 @@ import numpy as np
 
 
 class Softmax:
+    """This class implements the softmax which maps the Fully Connected Layer's output to a probability distribution
+    for each of the labels."""
     def __init__(self):
         self.output = None
     
-    def forward(self, x):
-        self.output = np.exp(input) / np.sum(np.exp(input))
+    def forward(self, input):
+        exps = np.exp(input - np.max(input))
+        self.output = exps / np.sum(exps)
         return self.output
-    
-    def backward(self, output_grad):
-        pass
+
+    def backward(self, input, learning_rate):
+        jacob = np.diag(self.output) - np.outer(self.output, self.output)
+        return np.dot(jacob, input)
