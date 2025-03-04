e670355a4d0d98977fe373256ed1317d762076c4
Aaryan Patel <aaryangpat3l@gmail.com>
2025-03-04 16:46:52 -0500

    Final
    Comments and reformatting

--- Activation/Tanh.py
+++ Activation/Tanh.py
@@ -2,8 +2,13 @@ import numpy as np
 
 
 class Tanh:
+    """This class implements the Hyperbolic Tangent (Tanh) activation function used throughout the
+    network."""
+
+    """Tanh activation function for forward propagation."""
     def forward(self, input):
         return (np.exp(input) - np.exp(-input)) / (np.exp(input) + np.exp(-input))
-    
+
+    """Back propagation of Tanh."""
     def backward(self, input):
         return 1 - self.forward(self.forward(input)) ** 2
