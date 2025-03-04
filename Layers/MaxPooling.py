c69618477cc4193da12d5caae8b314b6029614cc
Aaryan Patel <aaryangpat3l@gmail.com>
2025-03-04 16:48:49 -0500

    Comments

--- Layers/MaxPooling.py
+++ Layers/MaxPooling.py
@@ -4,11 +4,11 @@ import math
 
 class MaxPooling:
     """This class implements the Max Pooling layer which reduces the image's dimensions."""
+
     def __init__(self, pool_size, stride):
         self.pool_size = pool_size
         self.stride = stride
         self.input = None
-
 
     """Reduces the dimensions of the input image by selecting the maximum element in each pool window."""
     def forward(self, input):
@@ -24,13 +24,13 @@ class MaxPooling:
             for row in range(0, input_height - self.pool_size + 1, self.stride):
                 for col in range(0, input_width - self.pool_size + 1, self.stride):
                     # Extracts the overlap of the pool window and the original input image
-                    overlap = self.input[channel, row : row + self.pool_size, col : col + self.pool_size]
+                    overlap = self.input[channel, row: row + self.pool_size, col: col + self.pool_size]
                     # Enter the maximum element into the output matrix with lower dimensions than the input image
                     output[channel, math.floor(row / self.stride), math.floor(col / self.stride)] = np.max(overlap)
 
         return output
 
-
+    """Updates the gradients of the max elements selected in the Max Pooling forward propagation."""
     def backward(self, output_grad, learning_rate):
         input_grad = np.zeros_like(self.input)
         input_depth, input_height, input_width = self.input.shape
@@ -39,10 +39,10 @@ class MaxPooling:
         for channel in range(input_depth):
             for row in range(0, input_height - self.pool_size + 1, self.stride):
                 for col in range(0, input_width - self.pool_size + 1, self.stride):
-                    overlap = self.input[channel, row : row + self.pool_size, col : col + self.pool_size]
-                    # Flattens to find the max element and then scales back to the window pool size
+                    overlap = self.input[channel, row: row + self.pool_size, col: col + self.pool_size]
+                    # Flattens to find the index of the max element and scales it to the index in the overlap region
                     max_index = np.unravel_index(np.argmax(overlap, axis=None), overlap.shape)
-                    # TODO: Adds maximum element to the calculated index
+                    # Adds corresponding element of output grad to the input grad at the max element's index
                     input_grad[channel, row + max_index[0], col + max_index[1]] = output_grad[channel, math.floor(row / self.stride), math.floor(col / self.stride)]
 
         return input_grad
