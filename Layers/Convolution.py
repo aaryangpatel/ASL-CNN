4905456629d3865d3e7a82323324dbe758a3a674
Aaryan Patel <aaryangpat3l@gmail.com>
2025-03-04 16:49:20 -0500

    Comments

--- Layers/Convolution.py
+++ Layers/Convolution.py
@@ -4,6 +4,7 @@ from scipy import signal
 
 class Convolution:
     """This class implements the convolutional stage of the CNN which emphasizes the image's central features."""
+
     def __init__(self, input_shape, output_depth, kernel_size):
         self.input_shape = input_shape
         self.output_depth = output_depth
@@ -26,22 +27,26 @@ class Convolution:
         self.input = input
         self.output = np.copy(self.biases)
 
+        # Iterate through each output channel and accumulate the valid correlation value of the input channel with the associated kernels
         for output_channel in range(self.output_depth):
             for input_channel in range(self.input_depth):
-                self.output[output_channel] += signal.correlate2d(self.input[input_channel], self.kernels[output_channel, input_channel], "valid")
+                self.output[output_channel] += signal.correlate2d(self.input[input_channel],
+                                                                  self.kernels[output_channel, input_channel], "valid")
 
         return self.output
 
-
-    """Computes the gradients and adjusts kernels and biases and the input"""
+    """Computes the gradients and adjusts kernels and biases and the input."""
     def backward(self, output_grad, learning_rate):
         kernel_grad = np.zeros_like(self.kernels)
         input_grad = np.zeros_like(self.input)
 
+        # Iterate through each output channel and accumulate the valid correlation value of the input channel with the associated kernels
         for output_channel in range(self.output_depth):
             for input_channel in range(self.input_depth):
-                kernel_grad[output_channel, input_channel] += signal.correlate2d(self.input[input_channel], output_grad[output_channel], "valid")
-                input_grad[input_channel] += signal.convolve2d(output_grad[output_channel], self.kernels[output_channel, input_channel], "full")
+                kernel_grad[output_channel, input_channel] += signal.correlate2d(self.input[input_channel],
+                                                                                 output_grad[output_channel], "valid")
+                input_grad[input_channel] += signal.convolve2d(output_grad[output_channel],
+                                                               self.kernels[output_channel, input_channel], "full")
 
         self.kernels -= learning_rate * kernel_grad
         self.biases -= learning_rate * output_grad
